350ac979b2cf861adee22f54d46b82c6068cad79
erlep <egix@centrum.cz>
2023-02-26 11:48:24 +0100

    while

--- Py34_empty .py
+++ Py34_empty .py
@@ -1,3 +1,19 @@
 ﻿#!/usr/bin/env python3
 
-# Ukázka pro Anika
+# Ukázka pro Anika - https://www.online-python.com
+
+# i = 1
+# while i <= 2:
+#   print('pro i ', i)
+#   j = 1
+#   while j <= 2:
+#     print('pro i ', i, 'pro j ', j, )
+#     j = j + 1
+#   i = i + 1
+
+i = 1
+while i < 6:
+  print(i)
+   if i == 3:
+      break
+    i += 1
