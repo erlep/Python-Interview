5059ad2f2c8d224e338d60289c1b5499b979286b
erlep <egix@centrum.cz>
2023-04-19 19:55:51 +0200

    Paja

--- Py35_empty.py
+++ Py35_empty.py
@@ -1,8 +1,16 @@
-﻿#!python3
+﻿#!python
 
-a = None
+# n = 4
+# for p in range(n):
+#   print(p)
 
-if a:
-  print('su tu')
-else:
-  print('false')
+a = 12.99
+b = 3.45
+print(str(int(a))+str(int(b)))
+
+c = float(int(str(int(12.34))))
+print(c, type(c))
+
+d = '1'
+e = '2'
+print(d + e, type(d+e))
