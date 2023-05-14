a0d6390f3672dd04be8d3859e383222cb2c8369e
erlep <egix@centrum.cz>
2023-05-14 17:07:21 +0200

    re.search vs. re.match

--- Py25_RegEx.py
+++ Py25_RegEx.py
@@ -7,7 +7,18 @@ import re
 # example code:
 my_string = '1   2        3   '
 
+# match
 parsed = re.match(r"(\d)\s+(\d)\s+(\d)", my_string)
+print('parsed:', parsed)
+if parsed:
+  print('RegEx nula: >>', parsed[0], '<<')
+  print('RegEx jedna', parsed[1], '  dva', parsed[2], '  tri', parsed[3])
+else:
+  print('Nic nenasel.')
+print()
+
+# search
+parsed = re.search(r"(\d)\s+(\d)\s+(\d)", my_string)
 print('parsed:', parsed)
 if parsed:
   print('RegEx nula: >>', parsed[0], '<<')
