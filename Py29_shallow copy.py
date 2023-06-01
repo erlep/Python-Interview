3e1be00c6e4cfc016a4a0d16417dbec3e213c0a5
zbpgmy <Pavel.Egerle@onsemi.com>
2023-06-01 12:56:13 +0200

    shallow copy

---
+++ Py29_shallow copy.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env python
+
+a = 1
+b = a
+print ('a',a)
+print ('b',b)
+a=2
+print ('a',a)
+print ('b',b)
+
+la = [1,2,3]
+lb = la  # crate new link to list
+# lb = la[:] # shallow copy
+print ('la',la)
+print ('lb',lb)
+la.append(4)
+print ('la',la)
+print ('lb',lb)
