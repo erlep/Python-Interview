fed474935a28c4f8880d92e33f0c649e096b7940
zbpgmy <Pavel.Egerle@onsemi.com>
2023-10-17 09:29:55 +0200

    unicode

--- Py33_empty.py
+++ Py33_empty.py
@@ -1,1 +1,17 @@
 ﻿#!/usr/bin/env python
+
+# Python code running in VS Code terminal but throwing error in output tab - https://bit.ly/3M4es83
+# vscode-code-runner output utf8 chars incorrect https://bit.ly/46U4sGC
+import emoji
+
+print(emoji.emojize('I :red_heart:  Python!'))
+
+# {
+#     "code-runner.runInTerminal": true
+# }
+
+# {
+#     "code-runner.executorMap": {
+#         "python": "set PYTHONIOENCODING=utf8 && python"
+#     }
+# }
