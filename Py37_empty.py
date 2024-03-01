b02f68d76c98556f9d10546c2dc8c57531b88d8f
zbpgmy <Pavel.Egerle@onsemi.com>
2024-03-01 18:18:01 +0100

    tmp

---
+++ Py37_empty.py
@@ -0,0 +1,75 @@
+﻿#!/usr/bin/env python
+
+# Type Annotations
+def upper_everything(elements: list[str]) -> list[str]:
+  return [element.upper() for element in elements]
+loud_list: list[str] = upper_everything(['Mario', 'James', 'Sandra'])
+print('loud_list', loud_list)
+# Type Annotations
+sample: list[str | int] = ['a', 1, 'b', 2]
+print('sample', sample, '\n')
+
+# List comprehensions
+names: list[str] = ['James', 'Charlotte', 'Stephany', 'Mario', 'Sandra']
+Long_names: list[str] = [name for name in names if len(name) > 7]
+print(f'Long names: {Long_names} ')
+# without variable
+print(f'Long names: {[name for name in names if len(name) > 7]} ')
+
+# 5 Uncommon Python Features I Love https://youtu.be/sQ1Q96-Vhjk?si=olMWn2-BDlf0n7Jc
+
+# Slice object
+numbers: list[int] = list(range(1, 11))
+text: str = 'Hello, world!'
+print(numbers[::-1])
+print(text[::-1])
+# ::-1
+rev: slice = slice(None, None, -1)
+print(numbers[rev])
+print(text[rev])
+print()
+
+# Sets
+set_a: set[int] = {1, 2, 3, 4, 5}
+set_b: set[int] = {4, 5, 6, 7, 8}
+# Combine: | → union (vertical bar char)
+print('| ', set_a | set_b)
+# & → intersection
+print('& ', set_a & set_b)
+# - ^ → difference/symmetric diff.
+print('- ', set_a - set_b)
+print('- ', set_b - set_a)
+# pouze unikatni = doplnek do pruniku
+print('^ ', set_a ^ set_b)
+# < <= > >= → inclusion relations
+print('<=', set_a <= set_b)
+print()
+
+# f-String for class
+class Book:
+  def __init__(self, title: str, pages: int) -> None:
+    self.title = title
+    self.pages = pages
+  def __format__(self, format_spec: any) -> str:
+    match format_spec:
+      case 'time':
+        return f'{self.pages / 60:.2f}h'
+      case 'caps':
+        return self. title.upper()
+      case _:
+        raise ValueError(f'Unknown specifier for Book()')
+
+def main() -> None:
+  hairy_potter: Book = Book('Very Hairy Potter', 300)
+  python_daily: Book = Book('Python Daily', 20)
+  print(f' {hairy_potter:caps}')
+  print(f'Read time: {hairy_potter:time}')
+
+  print(f' {python_daily:caps}')
+  print(f'Read time: {python_daily:time}')
+
+pokracovat  CAS 8:00
+# 5 Uncommon Python Features I Love https://youtu.be/sQ1Q96-Vhjk?si=olMWn2-BDlf0n7Jc
+
+if __name__ == "__main__":
+  main()
